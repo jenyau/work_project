df38cb27c5eab7881d4a6cbc4555c97dc3880176
jenyau <jekharitonov@gmail.com>
2020-05-13 16:07:35 +0300

    all task complete branch develop

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,6 +7,8 @@ class Task < ApplicationRecord
 
   enum status: %i[complete failed in_progress]
   scope :total_complete, -> { where(status: 'complete').length }
-  scope :percent, -> { ((where(status: 'complete').length.to_f / (where(status: 'complete').
-                         length + where(status: 'failed').length).to_f) * 100).to_i }
+  scope :percent, lambda {
+                    ((where(status: 'complete').length.to_f / (where(status: 'complete')
+                         .length + where(status: 'failed').length).to_f) * 100).to_i
+                  }
 end
